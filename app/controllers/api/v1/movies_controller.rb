59ac9a502da1b7308490a36e6963e6b6b668eb3e
Awais Ilyas <awais545@gmail.com>
2013-12-08 19:39:45 +0500

    movies controller with update and destroy

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -14,4 +14,24 @@ class Api::V1::MoviesController < ApplicationController
       render json: @movie.errors, status: 404
     end
   end
+
+  # PUT /api/v1/movies/:id
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update_attributes(params[:movie])
+      render json: @movie, status: 200
+    else
+      render json: @movie.errors, status: 422
+    end
+  end
+
+  # DELETE /api/v1/movies/:id
+  def destroy
+    @movie = Movie.find_by_id(params[:id])
+    return render :json => { :errors => "Movie not found" }, status: 404 unless @movie
+    
+    @movie.destroy
+    render json: @movie, status: 204 #no_content
+  end
 end
