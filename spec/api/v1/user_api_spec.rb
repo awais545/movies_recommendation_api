dc54fb8f88691a78798441cf3ad43f9780fc11aa
Awais Ilyas <awais545@gmail.com>
2013-12-08 18:20:09 +0500

    spec: update user

--- spec/api/v1/user_api_spec.rb
+++ spec/api/v1/user_api_spec.rb
@@ -9,5 +9,40 @@ describe 'User Api' do
         response.status.should == 201
       end    
     end
+
+    context "with invalid attributes" do
+      it "should not create the user" do
+        User.any_instance.stub(:save).and_return(false)
+        post api_v1_users_path, FactoryGirl.attributes_for(:user).to_json
+        response.status.should == 404
+      end
+    end
+
+    context "with invalid format" do
+      it "should give error message" do
+        post api_v1_users_path, FactoryGirl.attributes_for(:user)
+        response.status.should == 400
+        JSON.parse(response.body)['message'].should == 'Invalid JSON payload'
+      end    
+    end
+  end
+
+  describe "PUT update" do
+    context "with valid attributes" do
+      it "should allow to update user" do
+        user = FactoryGirl.create :user
+        put api_v1_user_path(user), { name: 'Awais' }.to_json
+        response.status.should == 200
+      end
+    end
+
+    context "with invalid attributes" do
+      it "should not allow to update user" do
+        user = FactoryGirl.create :user
+        User.any_instance.stub(:update_attributes).and_return(false)
+        put api_v1_user_path(user), { name: 'Awais' }.to_json
+        response.status.should == 422
+      end
+    end
   end
 end
