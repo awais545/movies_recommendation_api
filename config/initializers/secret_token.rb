309c71211cfceb29c38c35f2cbec2460645e2012
Awais Ilyas <awais545@gmail.com>
2013-12-08 17:45:14 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieRecommendationSystem::Application.config.secret_token = '3f72b03e1f457ae3dc34b8bfe509cf6cc89532f57b95a480ef56bdd38d51e3d74d2418ae2f7c981584f92fe6e5a7a1178872be286836b0de5170fb1a4f05d6a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MovieRecommendationSystem::Application.config.session_store :cookie_store, key: '_movie_recommendation_system_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MovieRecommendationSystem::Application.config.session_store :active_record_store
