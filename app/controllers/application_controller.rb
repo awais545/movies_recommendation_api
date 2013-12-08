18027448cd622ff5e7ef8b325732006211afeb18
Awais Ilyas <awais545@gmail.com>
2013-12-08 22:14:54 +0500

    added http based authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
+	http_basic_authenticate_with name: 'kurobase', password: 'kurobase_awais_api'
+
 	def parse_json_request
 		JSON.load(request.raw_post)
 	rescue
