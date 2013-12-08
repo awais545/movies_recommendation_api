c63b00123a8cc7763598097d5f5a17e60a265a83
Awais Ilyas <awais545@gmail.com>
2013-12-08 20:33:02 +0500

    like model validations and associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,9 @@
 class Like < ActiveRecord::Base
-  # attr_accessible :title, :body
+	attr_accessible :user_id, :movie_id
+
+	belongs_to :user
+  belongs_to :movie
+
+	validates :movie_id, :user_id, presence: true
+	validates :movie_id, uniqueness: { scope: :user_id, message: 'You have already like this movie' }
 end
