216795999aab3927ad74788beda1627adb902d96
Awais Ilyas <awais545@gmail.com>
2014-05-26 17:05:24 +0500

    update rating after movie creation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :genres, :release_year, :title
+  attr_accessible :genres, :release_year, :title, :rating
 
 	include Tire::Model::Search
 	include Tire::Model::Callbacks
@@ -13,18 +13,25 @@ class Movie < ActiveRecord::Base
 
   validates :title, presence: true
 
+  after_create :map_imdb_rating
+
 	def self.search(params)
 		tire.search do
 			query { string params[:query], default_operator: "AND" } if params[:query].present?
+			
 			if params[:upcoming_movies].blank?
 				filter :range, release_year: { lte: Time.zone.now.year } 
 			else
-				filter :range, release_year: { gt: Time.zone.now.year } 
+				filter :range, release_year: { gt: Time.zone.now.year }
 			end
 		end
 	end
 
 	def to_indexed_json
  		to_json
+ 	end
+
+ 	def map_imdb_rating
+ 		ImdbWorker.perform_async(self.title)
  	end
 end
